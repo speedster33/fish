d3687cd55a1451b11dd90f546a5fde4ec19d116d
Joshua Hammer <jph33@outlook.com>
2023-01-31 20:04:01 -0500

    first try

---
+++ config.fish
@@ -0,0 +1,25 @@
+set -g fish_greeting ""
+
+set fish_color_command green
+set fish_color_comment brblack
+set fish_color_cwd cyan
+set fish_color_end red
+set fish_color_error orange
+set fish_color_host blue
+set fish_color_operator brblue
+set fish_color_params brgreen
+set fish_color_quote grey
+set fish_color_redirection yellow
+set fish_color_user green
+
+fish_vi_key_bindings
+fish_vi_cursor
+
+alias vi nvim
+
+alias lab "/mnt/c/Users/jph33/OneDrive/Documents/School/bju/2023s/cps210/labs/"
+alias bju "/mnt/c/Users/jph33/OneDrive/Documents/School/bju/"
+alias doc "/mnt/c/Users/jph33/OneDrive/Documents/"
+alias jph "/mnt/c/Users/jph33/"
+
+fortune | cowsay | lolcat
