f3fd3d38bf5151bac66260d3526f65f1105dd5cb
Joshua Hammer <jph33@outlook.com>
2023-02-11 16:03:03 -0500

    hydro prompt config

--- config.fish
+++ config.fish
@@ -4,13 +4,16 @@ set fish_color_command green
 set fish_color_comment brblack
 set fish_color_cwd cyan
 set fish_color_end red
-set fish_color_error orange
+set fish_color_error yellow
 set fish_color_host blue
 set fish_color_operator brblue
 set fish_color_params brgreen
 set fish_color_quote grey
 set fish_color_redirection yellow
 set fish_color_user green
+
+#hydro prompt
+set hydro_fetch true
 
 fish_vi_key_bindings
 fish_vi_cursor
@@ -21,6 +24,3 @@ function kt
     kotlinc main.kt
     kotlin MainKt.class
 end
-
-
-fortune | cowsay | lolcat
